1e521a1d4c29437a4d3c39951d8c55bb627a7268
zengshuang5 <zengshuang5@yonyou.com>
2017-01-06 13:08:20 +0800

    修改生成二维码方法的参数

--- QrCode/src/test/QrCode.java
+++ QrCode/src/test/QrCode.java
@@ -13,9 +13,9 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
-public class Main {
+public class QrCode {
 
-	public static String createQrcode(String _text){
+	public static String createQrcode(String url){
         String qrcodeFilePath = "";
         try {
             int qrcodeWidth = 300;
@@ -23,11 +23,11 @@ public class Main {
             String qrcodeFormat = "png";
             HashMap<EncodeHintType, String> hints = new HashMap<EncodeHintType, String>();
             hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-            BitMatrix bitMatrix = new MultiFormatWriter().encode("http://www.baidu.com", BarcodeFormat.QR_CODE, qrcodeWidth, qrcodeHeight, hints);
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, qrcodeWidth, qrcodeHeight, hints);
 
             BufferedImage image = new BufferedImage(qrcodeWidth, qrcodeHeight, BufferedImage.TYPE_INT_RGB);
             Random random = new Random();
-            File QrcodeFile = new File("D:\\" + random.nextInt() + "." + qrcodeFormat);
+            File QrcodeFile = new File("C:\\" + random.nextInt() + "." + qrcodeFormat);
             ImageIO.write(image, qrcodeFormat, QrcodeFile);
             MatrixToImageWriter.writeToFile(bitMatrix, qrcodeFormat, QrcodeFile);
             qrcodeFilePath = QrcodeFile.getAbsolutePath();
@@ -38,6 +38,6 @@ public class Main {
     }
 	
 	public static void main(String[] args) {
-		new Main().createQrcode("");
+		System.out.println(createQrcode("www.baidu.com"));
 	}
 }
