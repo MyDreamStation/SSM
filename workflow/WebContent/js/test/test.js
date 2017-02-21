d1b5d206057b4af362cb373bca1d5dd3097823c5
zengshuang5 <zengshuang5@yonyou.com>
2017-02-21 15:39:06 +0800

    1

--- workflow/WebContent/js/test/test.js
+++ workflow/WebContent/js/test/test.js
@@ -1,6 +1,22 @@
 $(function(){
 	
 	
-	layer.alert("hello layer!");
+	$.ajax({
+		type:'get',
+		url:'http://20.1.75.42:8081/bpm/myhasdoWeb.do',
+		data:{
+			param:{
+				data:{
+					start:0,
+					pagesize:15
+				}
+			}
+		},
+		dataType:'json',
+		success:function(data){
+			console.log(data);
+			alert("success");	
+		}
+	})
 	
 })
