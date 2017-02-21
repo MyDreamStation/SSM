84dc0ae6af088ccc8303a30a2fc9fb3ed8f88fad
zengshuang5 <zengshuang5@yonyou.com>
2017-02-21 15:53:32 +0800

    123

--- TestActiviti/src/main/java/test/TestMailTask.java
+++ TestActiviti/src/main/java/test/TestMailTask.java
@@ -1,5 +1,30 @@
 package test;
 
+import org.activiti.engine.FormService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.repository.Deployment;
+
 public class TestMailTask {
 
+	
+	public static void main(String[] args) {
+		ProcessEngine engine=ProcessEngines.getDefaultProcessEngine();
+		
+		RepositoryService repositoryService = engine.getRepositoryService();
+		RuntimeService runtimeService = engine.getRuntimeService();
+		FormService formService = engine.getFormService();
+		TaskService taskService = engine.getTaskService();
+		
+		Deployment deploy = repositoryService.createDeployment().addClasspathResource("testMailProcess.bpmn20.xml").deploy();
+		
+		runtimeService.startProcessInstanceByKey("testMailProcess");
+		
+		
+		
+	}
+	
 }
