7ed93538fe9b8c8ed7de9d39dc0e22c4e3227054
zengshuang5 <zengshuang5@yonyou.com>
2017-03-02 16:57:24 +0800

    1

--- workflow/src/main/java/com/bjtu/zs/util/FixedBasicDataSource.java
+++ workflow/src/main/java/com/bjtu/zs/util/FixedBasicDataSource.java
@@ -1,25 +1,27 @@
 package com.bjtu.zs.util;
+
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import org.apache.commons.dbcp.BasicDataSource;
 
 public class FixedBasicDataSource extends BasicDataSource {
-	
+
 	@Override
 	public <T> T unwrap(Class<T> iface) throws SQLException {
-	  // TODO Auto-generated method stub
-	  return null;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public boolean isWrapperFor(Class<?> iface) throws SQLException {
-	  // TODO Auto-generated method stub
-	  return false;
+		// TODO Auto-generated method stub
+		return false;
 	}
-	@Override 
-	    public synchronized void close() throws SQLException { 
-	        DriverManager.deregisterDriver(DriverManager.getDriver(url)); 
-	        super.close(); 
-	    }  
+
+	@Override
+	public synchronized void close() throws SQLException {
+		DriverManager.deregisterDriver(DriverManager.getDriver(url));
+		super.close();
+	}
 }
