55327289d8a1d814e41c9ce009434613f3cae697
zengshuang5 <zengshuang5@yonyou.com>
2017-03-02 15:43:52 +0800

    流程接口初待完善版本

--- workflow/src/main/java/com/bjtu/zs/service/ProcessService.java
+++ workflow/src/main/java/com/bjtu/zs/service/ProcessService.java
@@ -1,6 +1,21 @@
 package com.bjtu.zs.service;
 
+/**
+ * @ClassName ProcessService
+ * @description 流程服务接口
+ * @author zengshuang
+ * @date 2017年3月2日15:39:03
+ */
 public interface ProcessService {
 
-	
+	/**
+	 * 通过流程定义的id启动流程
+	 * 
+	 * @param id:流程定义中的id
+	 */
+	public void startProcessById(String id);
+
+
+	public void getTodo();
+
 }
