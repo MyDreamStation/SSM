5d1d84c41594b494918a4fc0d0e1640ee2969cda
zengshuang5 <zengshuang5@yonyou.com>
2017-03-09 10:50:35 +0800

    邮件实体bean（待完善）

---
+++ workflow/src/main/java/com/bjtu/zs/util/MailEntity.java
@@ -0,0 +1,27 @@
+package com.bjtu.zs.util;
+
+public class MailEntity {
+
+	//发件人
+	private String from;
+	
+	//收件人
+	private String to;
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+	
+}
