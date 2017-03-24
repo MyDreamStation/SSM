a4d98df279bbe4412ecca3ef2ac825e83eb768f0
zengshuang5 <zengshuang5@yonyou.com>
2017-03-24 09:42:31 +0800

    navbar.js

--- workflow/WebContent/js/navbar.js
+++ workflow/WebContent/js/navbar.js
@@ -1,4 +1,3 @@
-/** navbar.js By Beginner Emain:zheng_jinfan@126.com HomePage:http://www.zhengjinfan.cn */
 layui.define(['element', 'common'], function(exports) {
 	"use strict";
 	var $ = layui.jquery,
